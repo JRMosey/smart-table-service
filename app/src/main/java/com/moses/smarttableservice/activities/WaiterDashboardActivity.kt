018efbbb7ffaa0c3a974b29ab9b60027e2855b29
JRMosey <kasidim7@gmail.com>
2026-05-26 11:31:01 -0400

    Setup project architecture and Firebase integration

---
+++ app/src/main/java/com/moses/smarttableservice/activities/WaiterDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.moses.smarttableservice.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.moses.smarttableservice.R
+
+class WaiterDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiter_dashboard)
+    }
+}
