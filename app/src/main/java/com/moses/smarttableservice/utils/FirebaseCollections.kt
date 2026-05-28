e36989cfa7a560d75465a1b57f8f21ebfa55585d
JRMosey <kasidim7@gmail.com>
2026-05-27 21:07:40 -0400

    Setup Firebase collections, models, and architecture

--- app/src/main/java/com/moses/smarttableservice/utils/FirebaseCollections.kt
+++ app/src/main/java/com/moses/smarttableservice/utils/FirebaseCollections.kt
@@ -1,4 +1,13 @@
 package com.moses.smarttableservice.utils
 
-class FirebaseCollections {
+object FirebaseCollections {
+
+    const val USERS = "users"
+    const val TABLES = "tables"
+    const val MENU_ITEMS = "menuItems"
+    const val ORDERS = "orders"
+    const val PAYMENTS = "payments"
+    const val NOTIFICATIONS = "notifications"
+    const val RESTAURANT_SETTINGS = "restaurantSettings"
+
 }
